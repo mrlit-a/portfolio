00715378379a6a683d080402e63a0be1ed1f2e95
andybro <132149967+mrlit-a@users.noreply.github.com>
2024-08-17 21:31:54 -0400

    Delete Portfolio.elegant directory

--- Portfolio.elegant/assets/js/portfolio.js
+++
@@ -1,8 +0,0 @@
-$('.btn__category').click(function () {
-    if(!$(this).hasClass('active')) {
-        $('.btn__category').removeClass('active');
-        $(this).addClass('active');
-        $('.portfolio__cards').fadeOut();
-        $('#category-'+$(this).data('category-index')).fadeIn();
-    }
-});
